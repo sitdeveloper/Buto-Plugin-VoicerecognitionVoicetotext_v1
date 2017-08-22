4b7cfd2254c45e4cb873eb93458d079fed1fd0b9
Claes Östlund <claes@stenbergit.se>
2017-08-22 13:49:20 +0200

    Small changes.

--- public/plugin_voicerecognition_vocietotext_v1.js
+++ public/plugin_voicerecognition_vocietotext_v1.js
@@ -1,36 +1,33 @@
 function plugin_voicerecognition_voicetotext_v1(){
-  var self = this;
-  var started = false;
+  var self      = this;
+  var started   = false;
   var target_id = null;
-  var lang = 'en-US';
+  var lang      = 'en-US';
   var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
-  var onstart = function(){}
-  var onend = function(){}
+  var onstart =      function(){}
+  var onend =        function(){}
   var onsoundstart = function(){}
-  var onsoundend = function(){}
+  var onsoundend =   function(){}
   var onaudiostart = function(){}
-  var onaudioend = function(){}
-  var onresult = function(){}
+  var onaudioend =   function(){}
+  var onresult =     function(){}
   var text = "";
-  this.setTargetId = function(v){target_id = v;}
-  this.setLang = function(v){lang=v;}
-  this.onstart = function(v){onstart=v;}
-  this.onend = function(v){onend=v;}
+  this.setTargetId =  function(v){target_id = v;}
+  this.setLang =      function(v){lang=v;}
+  this.onstart =      function(v){onstart=v;}
+  this.onend =        function(v){onend=v;}
   this.onsoundstart = function(v){onsoundstart=v;}
-  this.onsoundend = function(v){onsoundend=v;}
+  this.onsoundend =   function(v){onsoundend=v;}
   this.onaudiostart = function(v){onaudiostart=v;}
-  this.onaudioend = function(v){onaudioend=v;}
-  this.onresult = function(v){onresult=v;}
-  this.test = function(){
-    console.log(target_id);
-    console.log(lang);
-  }
-  this.start = function(){
+  this.onaudioend =   function(v){onaudioend=v;}
+  this.onresult =     function(v){onresult=v;}
+  this.start =        function(){
     if(started){
       // Already started!
     }else{
       recognition.continuous = true;
       recognition.interimResults = true;
+      recognition.lang = lang;
       recognition.onstart      = function(event) {onstart(); started = true;}
       recognition.onend        = function(event) {onend();   recognition.start();} //
       recognition.onsoundstart = function(event) {onsoundstart();}
