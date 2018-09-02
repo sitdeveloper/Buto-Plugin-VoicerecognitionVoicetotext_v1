1572e414db31a40b28a5dfc3c6893fa24d66480e
Claes Östlund <claesostlund@Mefromhd-MBP18.local>
2018-09-02 10:05:20 +0200

    Add param last_word.

--- public/plugin_voicerecognition_voicetotext_v1.js
+++ public/plugin_voicerecognition_voicetotext_v1.js
@@ -3,6 +3,7 @@ function plugin_voicerecognition_voicetotext_v1(){
   var started   = false;
   var target_id = null;
   var lang      = 'en-US';
+  var last_word = null;
   var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
   var onstart =      function(){}
   var onend =        function(){}
@@ -13,6 +14,7 @@ function plugin_voicerecognition_voicetotext_v1(){
   var onresult =     function(){}
   var command = [{word: ['speech'], method: function(){alert('You said speech!');}}, {word: ['buto'], method: function(){alert('You said buto!');}}]
   var text = "";
+  var form_input = false;
   this.setTargetId =  function(v){target_id = v;}
   this.setLang =      function(v){lang=v;}
   this.onstart =      function(v){onstart=v;}
@@ -23,6 +25,7 @@ function plugin_voicerecognition_voicetotext_v1(){
   this.onaudioend =   function(v){onaudioend=v;}
   this.onresult =     function(v){onresult=v;}
   this.setCommand =   function(v){command = v;}
+  this.setFormInput =  function(v){form_input = v;}
   this.start =        function(){
     if(started){
       // Already started!
@@ -31,9 +34,12 @@ function plugin_voicerecognition_voicetotext_v1(){
       recognition.interimResults = true;
       recognition.lang = lang;
       recognition.onstart      = function(event) {onstart(); started = true;}
-      recognition.onend        = function(event) {onend();   recognition.start();} //
+      recognition.onend        = function(event) {onend();   recognition.start();}
       recognition.onsoundstart = function(event) {onsoundstart();}
-      recognition.onsoundend   = function(event) {onsoundend(); text += "<hr>"; }
+      recognition.onsoundend   = function(event) {
+        onsoundend();
+        text += "<hr>";
+      }
       recognition.onaudiostart = function(event) {onaudiostart();}
       recognition.onaudioend   = function(event) {onaudioend();}
       recognition.onresult   = function(event) {self.handle_result(); onresult();}
@@ -42,35 +48,56 @@ function plugin_voicerecognition_voicetotext_v1(){
   }
   this.handle_result = function(){
     if(started){
-      /**
-       * Run methods depending on last word.
-       */
-      var word = event.results[event.results.length-1][0].transcript.trim().toLowerCase();
-      for(var i=0;i<command.length;i++){
-        for(var j=0;j<command[i].word.length;j++){
-          if(command[i].word[j]==word){
-            command[i].method();
-            break;
-          }
+      var last_word = null;
+      var text_last = "";
+      for(var i=event.resultIndex; i<event.results.length; i++){
+        var transcript = event.results[i][0].transcript;
+        transcript.replace("\n", "<br>");
+        if(event.results[i].isFinal){
+          text += " "+transcript;
+          last_word = transcript.trim().toLowerCase();
         }
-      }
-      /**
-       * Set words in a element.
-       */
-      if(target_id){
-        var text_last = "";
-        for(var i=event.resultIndex; i<event.results.length; i++){
-          var transcript = event.results[i][0].transcript;
-          transcript.replace("\n", "<br>");
-          if(event.results[i].isFinal){
-            text += " "+transcript;
-          }
-          else{
-            text_last += transcript;
-          }
+        else{
+          text_last += transcript;
+        }
+        /**
+         * Set words in a element.
+         */
+        if(target_id){
           document.getElementById(target_id).innerHTML = text + ' <span style="color: #999;">' + text_last + '</span>';
         }
       }
+      /**
+       * Run methods depending on last word.
+       */
+      if(last_word){
+        for(var i=0;i<command.length;i++){
+          for(var j=0;j<command[i].word.length;j++){
+            if(command[i].word[j]==last_word){
+              command[i].method();
+              break;
+            }
+          }
+        }
+      }
+      /**
+       * Set last word in field.
+       */
+      if(form_input && last_word){
+        if(document.activeElement.tagName=='INPUT'){
+          if(document.activeElement.value.length==0){
+            document.activeElement.value = last_word;
+          }
+        }
+        if(document.activeElement.tagName=='TEXTAREA'){
+          if(document.activeElement.innerHTML.length){
+            document.activeElement.innerHTML += " "+last_word;
+          }else{
+            document.activeElement.innerHTML = last_word;
+          }
+        }
+      }
+      this.last_word = last_word;
     }
   }
 }
